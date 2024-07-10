685032d3e99e52d082fcf23c6a076dbc3d160831
unvisr-code <unvisr@icloud.com>
2024-07-10 11:54:33 +0900

    f

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const NOTION_API_KEY = 'secret_IBZ0cQY55Y5ZUlGQK7Jx3gziy8RIIljenkvpgvqmKhw';
 const DATABASE_ID = 'fce96a586958411d8a0b153a1563a75a';
 
-
 // 페이지가 로드되면 데이터를 가져오는 함수 실행
 document.addEventListener('DOMContentLoaded', fetchNotionData);
 
@@ -37,18 +36,21 @@ async function fetchNotionData() {
             const introductionCell = document.createElement('td');
             const descriptionCell = document.createElement('td');
 
+            // 각 셀에 값을 할당
             clubNameCell.textContent = page.properties['동아리명']?.title?.[0]?.plain_text || '';
             representativeCell.textContent = page.properties['대표자 성함']?.rich_text?.[0]?.plain_text || '';
             clubRoomAddressCell.textContent = page.properties['동아리방 주소']?.rich_text?.[0]?.plain_text || '';
             introductionCell.textContent = page.properties['한줄소개']?.rich_text?.[0]?.plain_text || '';
             descriptionCell.textContent = page.properties['Description']?.rich_text?.[0]?.plain_text || '';
 
+            // 각 셀을 행에 추가
             row.appendChild(clubNameCell);
             row.appendChild(representativeCell);
             row.appendChild(clubRoomAddressCell);
             row.appendChild(introductionCell);
             row.appendChild(descriptionCell);
 
+            // 행을 테이블 본문에 추가
             tableBody.appendChild(row);
         });
     } catch (error) {
