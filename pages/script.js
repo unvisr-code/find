a843a47baa0d01686a67d5983c8d55e88f5ee577
unvisr-code <unvisr@icloud.com>
2024-07-11 19:11:47 +0900

    roll back

--- pages/script.js
+++ pages/script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     findClubButton.addEventListener('click', () => {
         showLoadingAnimation();
-        fetchNotionData().then(() => {
+        setTimeout(() => {
             window.location.href = '/index.html';
-        });
+        }, 1000); // 애니메이션 시간과 일치하도록 설정
     });
 
     function showLoadingAnimation() {
@@ -13,19 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
         loadingDiv.className = 'loading-animation';
         loadingDiv.innerHTML = '<p>로딩 중...</p>';
         document.body.appendChild(loadingDiv);
-    }
-
-    async function fetchNotionData() {
-        try {
-            const response = await fetch('/api/fetchNotionData');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            console.log('Notion Data:', data);
-            // 데이터를 저장하거나 처리하는 로직 추가
-        } catch (error) {
-            console.error('Failed to fetch data:', error);
-        }
     }
 });
