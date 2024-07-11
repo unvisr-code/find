6c27696d9008fe71815063332b7c07c4bc5c46c1
unvisr-code <unvisr@icloud.com>
2024-07-11 18:58:45 +0900

    pages

---
+++ pages/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const findClubButton = document.getElementById('findClubButton');
+
+    findClubButton.addEventListener('click', () => {
+        showLoadingAnimation();
+        setTimeout(() => {
+            window.location.href = '/index';
+        }, 1000); // 애니메이션 시간과 일치하도록 설정
+    });
+
+    function showLoadingAnimation() {
+        const loadingDiv = document.createElement('div');
+        loadingDiv.className = 'loading-animation';
+        loadingDiv.innerHTML = '<p>로딩 중...</p>';
+        document.body.appendChild(loadingDiv);
+    }
+});
